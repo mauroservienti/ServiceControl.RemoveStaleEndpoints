9d96fb2454bee38fe595da97f1109b7bcaf78367
Mauro Servienti <mauro.servienti@gmail.com>
2024-03-12 18:01:29 +0100

    Add delete ServiceControl endpoints command

--- src/RemoveStaleEndpoints/App.cs
+++ src/RemoveStaleEndpoints/App.cs
@@ -4,68 +4,33 @@ namespace RemoveStaleEndpoints;
 
 static class App
 {
-    const string serviceControlEndpointsRoute = "api/endpoints";
-    const string serviceControlDeleteEndpointRoute = "api/endpoints/{endpointId}";
-    
-    // public static async Task PurgeServiceControlInactiveEndpoints(Uri serviceControlUri, TimeSpan cutoff)
-    // {
-    //     var client = new HttpClient()
-    //     {
-    //         BaseAddress = serviceControlUri
-    //     };
-    //
-    //     var toDelete = await GetEndpointsToDelete(client, serviceControlEndpointsRoute, cutoff);
-    //     await DeleteStaleEndpoints(toDelete, serviceControlDeleteEndpointRoute, client);
-    // }
-    //
-    // static async Task DeleteStaleEndpoints(List<EndpointStatus> toDelete, string deleteEndpointRoute, HttpClient client)
-    // {
-    //     foreach (var endpointToDelete in toDelete)
-    //     {
-    //         Console.WriteLine($"Deleting endpoint {endpointToDelete.Name}.");
-    //         var deleteUrl = deleteEndpointRoute.Replace("{endpointId}", endpointToDelete.Id);
-    //         await client.DeleteAsync(deleteUrl);
-    //         Console.WriteLine($"Endpoint {endpointToDelete.Name} deleted.");
-    //     }
-    // }
+    public static async Task PurgeServiceControlInactiveEndpoints(Uri serviceControlUri, TimeSpan cutoff)
+    {
+        var client = new HttpClient()
+        {
+            BaseAddress = serviceControlUri
+        };
 
-    // static async Task<List<EndpointStatus>> GetEndpointsToDelete(HttpClient client)
-    // {
-    //     var inactiveEndpoints = await GetInactiveEndpoints(client);
-    //     var endpointsResponse = await client.GetAsync(endpointsRoute);
-    //     var endpointsResponseString = await endpointsResponse.Content.ReadAsStringAsync();
-    //     var endpoints = JsonSerializer.Deserialize<EndpointStatus[]>
-    //     (
-    //         endpointsResponseString,
-    //         new JsonSerializerOptions()
-    //         {
-    //             PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
-    //         }
-    //     );
-    //
-    //     var toDelete = new List<EndpointStatus>();
-    //
-    //     if (endpoints != null)
-    //     {
-    //         foreach (var endpoint in endpoints)
-    //         {
-    //             Console.WriteLine($"{endpoint.Name}/{endpoint.Id} -> {endpoint.HeartbeatInformation.ReportedStatus}");
-    //             if (endpoint.HeartbeatInformation.ReportedStatus == "dead" &&
-    //                 endpoint.HeartbeatInformation.LastReportAt < DateTime.Now.Subtract(cutoff))
-    //             {
-    //                 Console.WriteLine($"Going to delete endpoint {endpoint.Name}.");
-    //                 toDelete.Add(endpoint);
-    //             }
-    //         }
-    //     }
-    //
-    //
-    //     return toDelete;
-    // }
-    
+        var inactiveEndpoints = await GetInactiveEndpoints(client);
+        await DeleteStaleEndpoints(client, inactiveEndpoints, cutoff);
+    }
+
+    static async Task DeleteStaleEndpoints(HttpClient client, List<EndpointStatus> inactiveEndpoints, TimeSpan cutoff)
+    {
+        var endpointsToDelete = inactiveEndpoints.Where(status =>
+            status.HeartbeatInformation.LastReportAt < DateTime.Now.Subtract(cutoff));
+        foreach (var toDelete in endpointsToDelete)
+        {
+            Console.WriteLine($"Endpoint {toDelete.Name} reported the last heartbeat more than {cutoff} ago. And it's status is stale. Removing it...");
+            var deleteUrl = $"api/endpoints/{toDelete.Id}";
+            await client.DeleteAsync(deleteUrl);
+            Console.WriteLine($"Endpoint {toDelete.Name} removed.");
+        }
+    }
+
     static async Task<List<EndpointStatus>> GetInactiveEndpoints(HttpClient client)
     {
-        var endpointsResponse = await client.GetAsync(serviceControlEndpointsRoute);
+        var endpointsResponse = await client.GetAsync("api/endpoints");
         var endpointsResponseString = await endpointsResponse.Content.ReadAsStringAsync();
         var endpoints = JsonSerializer.Deserialize<EndpointStatus[]>
         (
@@ -79,7 +44,7 @@ static class App
         var inactive = endpoints?
             .Where(endpoint => endpoint.HeartbeatInformation.ReportedStatus == "dead")
             .ToList();
-        
+
         return inactive ?? [];
     }
 
@@ -89,12 +54,13 @@ static class App
         {
             BaseAddress = serviceControlUri
         };
-        
+
         var inactiveEndpoints = await GetInactiveEndpoints(client);
         foreach (var endpoint in inactiveEndpoints)
         {
             Console.WriteLine($"{endpoint.Name}/{endpoint.Id} -> {endpoint.HeartbeatInformation.ReportedStatus}");
-            Console.WriteLine($"\tEndpoint {endpoint.Name} is inactive, last reported at {endpoint.HeartbeatInformation.LastReportAt}.");
+            Console.WriteLine(
+                $"\tEndpoint {endpoint.Name} is inactive, last reported at {endpoint.HeartbeatInformation.LastReportAt}.");
         }
     }
 }
